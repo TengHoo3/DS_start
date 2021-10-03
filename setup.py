226a538761ca01faf7c19d8555fac954e2e30acc
TengHoo <tenghoo3@gmail.com>
2021-10-03 23:41:34 +0800

    v4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ USER_NAME = 'TengHoo3'
 
 setuptools.setup(
     name=f"DS-basic-start",
-    version="0.0.3",
+    version="0.0.4",
     author=USER_NAME,
     author_email="tenghoo3@gmail.com",
     description="Start your DS projects with some basic analysis using this package",
@@ -26,13 +26,12 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.7",
-    install_requires = [
+    install_requires=[
         "scipy",
         "numpy",
         "pandas",
         "matplotlib",
         "seaborn",
         'sklearn',
-        ]
-    )
-
+    ]
+)
